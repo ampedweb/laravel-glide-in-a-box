a0b1cac51e66ebd846e01b6bd8c14c1cb880d771
Matt Gorle <matt@gorle.co.uk>
2021-01-23 15:28:31 +0000

    Fix incorrect base_url in test context

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,14 @@
 
 namespace AmpedWeb\GlideInABox\Tests;
 
+use AmpedWeb\GlideInABox\Providers\GlideInABoxRoutesProvider;
 use AmpedWeb\GlideInABox\Providers\GlideServerServiceProvider;
 use AmpedWeb\GlideInABox\Providers\GlideSignatureValidationServiceProvider;
 use AmpedWeb\GlideInABox\Providers\SignatureServiceProvider;
+use Illuminate\Config\Repository;
+use Illuminate\Container\Container;
+use Illuminate\Routing\RouteCollection;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Config;
 use Monolog\Formatter\LineFormatter;
@@ -15,6 +20,10 @@ class TestCase extends \Orchestra\Testbench\TestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        /** @var Router $router */
+        $router = $this->app->get('router');
+
         Artisan::call('storage:link');
         // additional setup
         //Set our glideinabox config
@@ -24,7 +33,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
                 'signature_key'     => '9e83e05bbf9b5db17ac0deec3b7ce6cba983f6dc50531c7a919f28d5fb3696c3',
                 'cache_path_prefix' => '.cache',
                 'source'            => public_path('storage'),
-                'base_url'          => 'img',
+                'base_url'          => '',
                 'max_image_size'    => 50 * 50,
                 'presets'           => [
                     'small'  => [
@@ -47,7 +56,8 @@ class TestCase extends \Orchestra\Testbench\TestCase
         return [
             GlideServerServiceProvider::class,
             GlideSignatureValidationServiceProvider::class,
-            SignatureServiceProvider::class
+            SignatureServiceProvider::class,
+            GlideInABoxRoutesProvider::class
         ];
     }
 
