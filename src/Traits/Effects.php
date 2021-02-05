2270d6b834d17af7d29e37b512c0edc08460336e
Matt Gorle <3032672+mattgorle@users.noreply.github.com>
2021-02-05 08:57:02 +0000

    Add Effects trait and unit tests.

---
+++ src/Traits/Effects.php
@@ -0,0 +1,118 @@
+<?php
+
+
+namespace AmpedWeb\GlideInABox\Traits;
+
+use AmpedWeb\GlideInABox\Exceptions\InvalidFilterException;
+
+/**
+ * This trait exposes image effects functionality
+ *
+ * @link    https://glide.thephpleague.com/1.0/api/effects/
+ * @package AmpedWeb\GlideInABox\Traits
+ */
+trait Effects
+{
+    /**
+     * @property array $buildParams
+     */
+
+    /** @var string $FILTER_SEPIA Apply a "sepia" filter to the image */
+    public static $FILTER_SEPIA = 'sepia';
+
+    /** @var string $FILTER_GREYSCALE Apply a "greyscale" filter to the image */
+    public static $FILTER_GREYSCALE = 'greyscale';
+
+    /**
+     * Adds a blur effect to the image. Use values between 0 and 100.
+     *
+     * @param int $blur Blur - use values between 0 and 100
+     *
+     * @return Effects
+     */
+    public function blur(int $blur = 0)
+    {
+        $blur = max(0, $blur);
+        $blur = min($blur, 100);
+
+        $this->buildParams['blur'] = $blur;
+
+        return $this;
+    }
+
+    /**
+     * Applies a pixelation effect to the image.  Use values between 0 and 1000.
+     *
+     * @param int $pixelation Pixelation - use values between 0 and 1000
+     *
+     * @return Effects
+     */
+    public function pixel(int $pixelation = 0)
+    {
+        $pixelation = max(0, $pixelation);
+        $pixelation = min(1000, $pixelation);
+
+        $this->buildParams['pixel'] = $pixelation;
+
+        return $this;
+    }
+
+    /**
+     * Applies a pixelation effect to the image.  Use values between 0 and 1000.
+     *
+     * @param int $pixelation Pixelation - use values between 0 and 1000
+     *
+     * @return Effects|\AmpedWeb\GlideInABox\Util\GlideUrl
+     * @see Effects::pixel()
+     */
+    public function pixelate(int $pixelation = 0)
+    {
+        return $this->pixel($pixelation);
+    }
+
+    /**
+     * Applies a filter effect to the image.  Accepts greyscale or sepia.
+     *
+     * @param string $filter Filter to apply.  Acceptable values are:
+     *                       - 'greyscale',
+     *                       - 'sepia'.
+     *
+     *                       Or, you can use one of the static strings:
+     *                       - Effects::$FILTER_GREYSCALE,
+     *                       - Effects::$FILTER_SEPIA
+     *
+     * @return Effects
+     * @throws InvalidFilterException
+     */
+    public function filt(string $filter)
+    {
+        if ($filter !== static::$FILTER_GREYSCALE &&
+            $filter !== static::$FILTER_SEPIA) {
+            throw new InvalidFilterException();
+        }
+
+        $this->buildParams['filt'] = $filter;
+
+        return $this;
+    }
+
+    /**
+     * Applies a filter effect to the image.  Accepts greyscale or sepia.
+     *
+     * @param string $filter Filter to apply.  Acceptable values are:
+     *                       - 'greyscale',
+     *                       - 'sepia'.
+     *
+     *                       Or, you can use one of the static strings:
+     *                       - Effects::$FILTER_GREYSCALE,
+     *                       - Effects::$FILTER_SEPIA
+     *
+     * @return Effects|\AmpedWeb\GlideInABox\Util\GlideUrl
+     * @throws InvalidFilterException
+     * @see Effects::filt()
+     */
+    public function filter(string $filter)
+    {
+        return $this->filt($filter);
+    }
+}
---
+++ tests/Feature/EffectsTest.php
@@ -0,0 +1,91 @@
+<?php
+
+
+namespace AmpedWeb\GlideInABox\Tests\Feature;
+
+
+use AmpedWeb\GlideInABox\Exceptions\InvalidFilterException;
+use AmpedWeb\GlideInABox\Tests\TestCase;
+use AmpedWeb\GlideInABox\Traits\Effects;
+use AmpedWeb\GlideInABox\Util\GlideUrl;
+
+class EffectsTest extends TestCase
+{
+    /** @var GlideUrl */
+    protected $glideUrl;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->glideUrl = glide_url('cat.png');
+    }
+
+    public function testBlurIsFluent()
+    {
+        $this->assertSame($this->glideUrl, $this->glideUrl->blur(30));
+    }
+
+    public function testPixelIsFluent()
+    {
+        $this->assertSame($this->glideUrl, $this->glideUrl->pixel(30));
+    }
+
+    public function testFiltIsFluent()
+    {
+        $this->assertSame($this->glideUrl, $this->glideUrl->filt('greyscale'));
+    }
+
+    public function testBlurSetsCorrectValues()
+    {
+        $this->glideUrl->blur(45);
+        $this->assertEquals(45, $this->glideUrl->getParams()['blur']);
+    }
+
+    public function testPixelSetsCorrectValues()
+    {
+        $this->glideUrl->pixel(995);
+        $this->assertEquals(995, $this->glideUrl->getParams()['pixel']);
+    }
+
+    public function testFiltSetsCorrectValues()
+    {
+        $this->glideUrl->filt(Effects::$FILTER_SEPIA);
+        $this->assertEquals(Effects::$FILTER_SEPIA, $this->glideUrl->getParams()['filt']);
+    }
+
+    public function testBlurFlattensOutOfRangeValues()
+    {
+        $this->glideUrl->blur(-999);
+        $this->assertEquals(0, $this->glideUrl->getParams()['blur']);
+
+        $this->glideUrl->blur(999);
+        $this->assertEquals(100, $this->glideUrl->getParams()['blur']);
+    }
+
+    public function testPixelFlattensOutOfRangeValues()
+    {
+        $this->glideUrl->pixel(-999);
+        $this->assertEquals(0, $this->glideUrl->getParams()['pixel']);
+
+        $this->glideUrl->pixel(9999);
+        $this->assertEquals(1000, $this->glideUrl->getParams()['pixel']);
+    }
+
+    public function testFiltThrowsExceptionIfInvalidParameterIsPassed()
+    {
+        $this->expectException(InvalidFilterException::class);
+
+        $this->glideUrl->filt('foo');
+    }
+
+    public function testPixelateIsAliasOfPixel()
+    {
+        $this->assertSame($this->glideUrl->pixelate(45), $this->glideUrl->pixel(45));
+    }
+
+    public function testFilterIsAliasOfFilt()
+    {
+        $this->assertSame($this->glideUrl->filter(Effects::$FILTER_SEPIA),
+                          $this->glideUrl->filt(Effects::$FILTER_SEPIA));
+    }
+}
