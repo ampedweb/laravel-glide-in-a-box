9731cb8c66503be93071732a830f7bcccdd8dc59
Chris Powell <hosting@ampedwebdesign.co.uk>
2021-02-06 11:55:18 +0000

    Removing redundant commented code in routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,6 +9,3 @@ use Illuminate\Support\Facades\Route;
 Route::prefix(config('glideinabox.base_url'))->group(function () {
     Route::get('{path}', [GlideImageController::class, 'image'])->where('path', '.*')->name('glideinabox.image');
 });
-
-//Route::group(['prefix' => config('glideinabox.base_url')], function () {
-//});
