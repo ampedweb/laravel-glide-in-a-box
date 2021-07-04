c8c4942bc66d79b6490489d2e87ce4aae04858e7
Chris Powell <hosting@ampedwebdesign.co.uk>
2021-07-04 16:57:25 +0100

    Adding url/path parsing closures to helper to maintain backwards compatability. Updating composer.json to use packagist repo.

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,8 @@
 <?php
 
-use AmpedWeb\GlideInABox\Util\GlideUrl;
 use AmpedWeb\GlideUrl\FluentUrlBuilder;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
 use League\Glide\Urls\UrlBuilder;
 
 if (!function_exists('glide_url')) {
@@ -17,7 +17,25 @@ if (!function_exists('glide_url')) {
         if ($path === null) {
             return '';
         }
-        return (new FluentUrlBuilder(App::make(UrlBuilder::class)))->setPath($path);
+
+
+        $parsedPathClosure = function($path) {
+            $parsedPath = $path;
+            if (Str::startsWith($parsedPath, '/storage/')) {
+                $parsedPath = Str::replaceFirst('/storage/', '', $parsedPath);
+            }
+            return Str::of($parsedPath)->replace('\\', '/');
+        };
+
+        $urlClosure = function($url) {
+          return url($url);
+        };
+
+
+        return (new FluentUrlBuilder(App::make(UrlBuilder::class)))
+                ->setPath($path)
+                ->setPathClosure($parsedPathClosure)
+                ->setUrlClosure($urlClosure);
     }
 
 }
